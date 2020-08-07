8d8904f928424b83d4f16ba867a18b8b086efbc1
Erfanch <erfan.chinif@gmail.com>
2020-08-07 15:40:49 -0700

    Jest test

---
+++ spec/index.test.js
@@ -0,0 +1,11 @@
+describe('index', () => {
+  it('should be true', () => {
+    const testVar = true;
+    expect(testVar).toBe(true)
+  });
+
+  it('should be false', () => {
+    const testVar = true;
+    expect(testVar).toBe(false)
+  });
+})
