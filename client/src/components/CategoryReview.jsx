51ee0057746c38b14585c8c168892f509361f0a6
Erfanch <erfan.chinif@gmail.com>
2020-08-09 12:14:45 -0700

    Review/Title component

---
+++ client/src/components/CategoryReview.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import styled from 'styled-components';
+import Stars from './Stars.jsx'
+import Title from './Title.jsx'
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-flow: row norwrap;
+  justify-content: space-between;
+  text-align: center;
+`;
+
+const Category = styled.div`
+  // flex: 1;
+  font-family: AdihausDIN,Helvetica,Arial,sans-serif;
+  font-weight: 400;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+`;
+
+const Review = styled.div`
+  display: flex;
+  align-item: center;
+  // flex: 1;
+  // flex-flow: row norwrap;
+  font-family: AdihausDIN,Helvetica,Arial,sans-serif;;
+  font-weight: 700;
+  // position: center;
+  // text-transform: uppercase;
+  letter-spacing: 2px;
+
+`;
+
+
+const CategoryReview = () => (
+  <div>
+    <Wrapper>
+      <Category>
+        women . original
+      </Category>
+      <Review>
+        <Stars />
+        <Stars />
+        <Stars />
+        <Stars />
+        <Stars />
+        7753
+      </Review>
+    </Wrapper>
+    <br></br>
+    <Title />
+
+  </div>
+)
+
+
+export default CategoryReview
---
+++ client/src/components/Title.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ProductName = styled.div`
+  display: block;
+  font-family: AdihausDIN Cn,Helvetica,Arial,sans-serif;
+  font-style: italic;
+  font-weight: 500;
+  font-size: 44px;
+  line-height: 38px;
+  text-transform: uppercase;
+`;
+
+const Color= styled.div`
+  display: block;
+  color: #000;
+  font-family: AdihausDIN,Helvetica,Arial,sans-serif;
+  font-size: 12px;
+  font-weight: 400;
+  letter-spacing: 2px;
+  line-height: 15px;
+  margin-bottom: 0;
+  text-transform: uppercase;
+`;
+
+const Title = () => (
+  <div>
+  <ProductName> SuperStar shoes</ProductName>
+  <br></br>
+  <Color>Cloud White / Core Black / Cloud White</Color>
+  </div>
+)
+
+export default Title;
