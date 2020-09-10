5d44a36c91fae8497f3ab56462dc3a60bfd1d4c4
Tom Nguyen <tommy.nguyen0721@gmail.com>
2020-09-09 22:27:03 -0700

    add postgres to server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 require('newrelic');
 const express = require('express');
-const model = require('../database/model.js');
+// const model = require('../database/model.js');
 const path = require('path');
 const app = express();
+// const pg = require('../newDatabase/Postgres/index.js');
+const user = require('../newDatabase/Postgres/User.js');
 const PORT = 3380;
 const expressStaticGzip = require('express-static-gzip');
 
@@ -13,29 +15,30 @@ app.use(expressStaticGzip(path.join(__dirname, '../client/dist'), { enableBrotli
 
 // * TODO *
 // extend existing API and create PUT and DELETE requests
+app.get('/sidebar/summary', user.readAll);
 
 app.get('/sidebar/summary', function (req, res) {
   // console.log(req, 'req body');
 
-  model.getOneProduct((err, results) => {
-    if (err) {
-      console.log('error', err);
-      res.status(400);
-    } else {
-      res.status(200).send(results)
-    }
-  })
+  // model.getOneProduct((err, results) => {
+  //   if (err) {
+  //     console.log('error', err);
+  //     res.status(400);
+  //   } else {
+  //     res.status(200).send(results)
+  //   }
+  // })
 })
 
 app.put('/sidebar/summary', (req, res) => {
-  model.updateProduct(req.body, (err, results) => {
-    if (err) {
-      console.log('err', err);
-      res.statuse(401)
-    } else {
-      res.status(201).end()
-    }
-  })
+  // model.updateProduct(req.body, (err, results) => {
+  //   if (err) {
+  //     console.log('err', err);
+  //     res.statuse(401)
+  //   } else {
+  //     res.status(201).end()
+  //   }
+  // })
 })
 
 
