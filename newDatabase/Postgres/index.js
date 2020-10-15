81f967e62c5e107a59f7332e770d29defe996584
Tom Nguyen <tommy.nguyen0721@gmail.com>
2020-10-15 10:51:05 -0700

    Clean up code

--- newDatabase/Postgres/index.js
+++ newDatabase/Postgres/index.js
@@ -5,18 +5,6 @@ const pool = new Pool({
   connectionString: DATABASE_URL,
 });
 
-// function query(text) {
-//   return new Promise((resolve, reject) => {
-//     pool
-//       .query(text)
-//       .then((res) => {
-//         resolve(res);
-//       })
-//       .catch((err) => {
-//         reject(err);
-//       });
-//   });
-// }
 
 let client = new Client({
   user: 'tomnguyen',
@@ -31,33 +19,3 @@ module.exports = {
   // query,
   client
 };
-// user: 'tomnguyen',
-//   host: 'localhost',
-//   database: 'sidebar',
-//   port: 5432,
-// let query = `SELECT * FROM inventory where shoe_id=405452 and color='BLACK'`;
-
-// pool.connect()
-//   .then( (client) => {
-//     client.query(query)
-//       .then( (res) => {
-//         for(let row of res.rows) {
-//           console.log(row)
-//         }
-//       })
-//       .catch( (err) => {
-//         console.error(err);
-//       })
-//   })
-//   .catch( (err) => {
-//     console.error(err);
-//   });
-
-//   pool.on('error', (err,client) => {
-//     console.error('Error:', err);
-//   })
-//   pool.once('open', () => {
-//     console.log('PostgreSQL connected!')
-//   })
-
-  // module.exports.pool = pool;
--- server/index.js
+++ server/index.js
@@ -4,21 +4,9 @@ const model = require('../database/model.js');
 const path = require('path');
 const app = express();
 const pg = require('../newDatabase/Postgres/index.js')
-// const user = require('../newDatabase/Postgres/User.js');
 const PORT = 3380;
 const expressStaticGzip = require('express-static-gzip');
-// const { Client } = require('pg');
 
-// let client = new Client({
-//   user: 'tomnguyen',
-//   host: 'localhost',
-//   database: 'sidebar',
-//   port: 5432,
-// });
-// client.connect();
-
-
-// app.use(express.static(__dirname + '/../client/dist'));
 app.use(express.json());
 app.use(expressStaticGzip(path.join(__dirname, '../client/dist'), { enableBrotli: true, orderPreference: ['br'] }));
 
@@ -63,7 +51,6 @@ app.put('/sidebar/summary', (req, res) => {
 
 
 app.post('/sidebar/summary', (req, res) => {
-  // should i update the database as well?
   let query = `insert into inventory(shoe_id,color,size,quantity) VALUES (${req.body.shoe_id},${req.body.color},${req.body.size},${req.body.quantity})`;
   console.log(req.body)
   pg.client
@@ -79,15 +66,5 @@ app.post('/sidebar/summary', (req, res) => {
 app.delete('/sidebar/summary', (req, res) => {
 
 })
-// app.post('/kix', (req, res) => {
-//   model.updateProductColor(req.body, (err, results) => {
-//     if (err) {
-//       console.log('err', err);
-//       res.statuse(401)
-//     } else {
-//       res.status(201).end()
-//     }
-//   })
-// })
 
 app.listen(PORT, () => console.log('Server is listening at port', PORT))
